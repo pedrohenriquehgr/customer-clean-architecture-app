b603ae58bc7fec15c6f9c700f83260655cbaea8e
Pedro Henrique Guimarães Reis <pedrohenriquehgr@gmail.com>
2024-04-10 00:29:17 -0300

    feat: Customer registrer from Kafka topic.

---
+++ src/main/kotlin/com/customer/infrastructure/adapters/messaging/kafka/CustomerRegisterStreamConfiguration.kt
@@ -0,0 +1,28 @@
+package com.customer.infrastructure.adapters.messaging.kafka
+
+import com.customer.infrastructure.adapters.executors.CustomerRegisterUseCaseExecutor
+import com.customer.infrastructure.adapters.rest.dto.CustomerRegisterRequest
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.function.Consumer
+
+
+@Configuration
+class CustomerRegisterStreamConfiguration(
+    private val customerRegisterUseCaseExecutor: CustomerRegisterUseCaseExecutor
+) {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    @Bean
+    fun customerRegisterStream(): Consumer<CustomerRegisterRequest> {
+        return Consumer<CustomerRegisterRequest> { customerRegisterRequest ->
+            try {
+                val customerRegisterResponse = customerRegisterUseCaseExecutor.execute(customerRegisterRequest)
+                log.info("Customer created response={}", customerRegisterResponse)
+            } catch (e: Throwable) {
+                log.error("Customer created error", e)
+            }
+        }
+    }
+}
